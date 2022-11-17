eb0596bd31681e49258177dedbc8b05f49133b57
hamed.sh <hsee1372@gmail.com>
2022-11-17 19:09:21 +0330

    github pages

--- mrbin_photo_componnent/README.md
+++
@@ -1,3 +0,0 @@
-#  npm install    first step
-after command take a few minute      size 200mb\
-# npm start 
--- mrbin_photo_componnent/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import App from "./componnent/app";
-
-
-const rootElement = document.getElementById("root");
-ReactDom.render(<App />, rootElement);
